3e0a2772580e86e35f2f51e91318e8e53f401bec
EnzanIjuin <protomanexe23@gmail.com>
2015-03-14 22:02:18 -0700

    Completed UI

--- app/src/main/java/com/example/shelton/calculator/MainActivity.java
+++ app/src/main/java/com/example/shelton/calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -14,11 +15,21 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     Button[] numbers = new Button[10];
     Button[] operators = new Button[6];
     Button[] edits = new Button[4];
+    TextView input, equation;
+
+    StringBuffer inBuffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inBuffer = new StringBuffer();
+
+        input = (TextView) findViewById(R.id.num);
+        equation = (TextView) findViewById(R.id.equation);
+        input.setText("");
+        equation.setText("");
 
         numbers[0] = (Button) findViewById(R.id.b0);
         numbers[1] = (Button) findViewById(R.id.b1);
@@ -46,7 +57,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         for(Button b : numbers) b.setOnClickListener(this);
         for(Button b : operators) b.setOnClickListener(this);
         for(Button b : edits) b.setOnClickListener(this);
-
     }
 
 
@@ -74,6 +84,84 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(this, "Button was clicked!", Toast.LENGTH_LONG).show();
+
+        switch(v.getId()) {
+            case R.id.b0:
+                input.setText(input.getText() + "0");
+                break;
+            case R.id.b1:
+                input.setText(input.getText() + "1");
+                break;
+            case R.id.b2:
+                input.setText(input.getText() + "2");
+                break;
+            case R.id.b3:
+                input.setText(input.getText() + "3");
+                break;
+            case R.id.b4:
+                input.setText(input.getText() + "4");
+                break;
+            case R.id.b5:
+                input.setText(input.getText() + "5");
+                break;
+            case R.id.b6:
+                input.setText(input.getText() + "6");
+                break;
+            case R.id.b7:
+                input.setText(input.getText() + "7");
+                break;
+            case R.id.b8:
+                input.setText(input.getText() + "8");
+                break;
+            case R.id.b9:
+                input.setText(input.getText() + "9");
+                break;
+            case R.id.b_point:
+                input.setText(input.getText() + ".");
+                break;
+            case R.id.b_plus:
+                equation.setText("" + equation.getText() + input.getText() + " + ");
+                inBuffer.append("+");
+                input.setText("");
+                break;
+            case R.id.b_minus:
+                equation.setText("" + equation.getText() + input.getText() + " - ");
+                inBuffer.append("-");
+                input.setText("");
+                break;
+            case R.id.b_times:
+                equation.setText("" + equation.getText() + input.getText() + " × ");
+                inBuffer.append("*");
+                input.setText("");
+                break;
+            case R.id.b_divide:
+                equation.setText("" + equation.getText() + input.getText() + " ÷ ");
+                inBuffer.append("/");
+                input.setText("");
+                break;
+            case R.id.b_mod:
+                equation.setText("" + equation.getText() + input.getText() + " % ");
+                inBuffer.append("%");
+                input.setText("");
+                break;
+            case R.id.b_clear:
+                input.setText("");
+                equation.setText("");
+                break;
+            case R.id.b_backspace:
+                input.setText(input.getText().subSequence(0, input.getText().length() - 1));
+                break;
+            case R.id.b_sign:
+                if(input.getText().charAt(0) != '-') {
+                    input.setText("-" + input.getText());
+                } else {
+                    input.setText(input.getText().subSequence(1, input.getText().length()));
+                }
+                break;
+            case R.id.b_equals:
+                equation.setText("" + equation.getText() + input.getText());
+                input.setText("Currently no expression evaluator =(");
+                break;
+        }
     }
 }
